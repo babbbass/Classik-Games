67ec36282735aed836115b5e804fafa4c85f4b62
sebastien <sebastien.savan@gmail.com>
2025-02-01 13:38:16 +0100

    style writing name game

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             <Link href={`/match/${match.id}`}>
               <Card className='text-slate-700 font-bold  hover:text-slate-800 transition-all duration-200 py-4 ease-in-out hover:scale-105 bg-gradient-to-b from-yellow-200 to-yellow-500 flex items-center md:w-1/2 mx-auto rounded-xl shadow-2xl'>
                 <CardContent className='flex flex-col items-center justify-center w-full text-base p-0'>
-                  <span>
+                  <span className='italic underline'>
                     {match.homeTeam.name} vs {match.awayTeam.name}
                   </span>{" "}
                   - <span>{match.competition}</span>
