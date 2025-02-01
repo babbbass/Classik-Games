9d17dd4a6b60e4b13ed99b79d41fa161685fbf35
sebastien <sebastien.savan@gmail.com>
2025-02-01 20:23:07 +0100

    smoothness for nav button

--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -6,7 +6,7 @@ export function NavButton({ text, href }: { text: string; href: string }) {
   return (
     <Link href={href}>
       <div className='w-3/4 max-w-sm flex justify-center mx-auto'>
-        <Button className='w-full bg-blue-500 rounded-full font-bold text-lg mt-2 hover:scale-110 hover:bg-blue-600'>
+        <Button className='w-full bg-blue-500 rounded-full font-bold text-lg mt-2 hover:scale-110 hover:bg-blue-600 transition-all duration-200 ease-in-out'>
           {text}
         </Button>
       </div>
