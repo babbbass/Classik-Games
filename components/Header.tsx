0fb05867f20ca0800945a089d8c8b2a5304260b2
sebastien <sebastien.savan@gmail.com>
2025-02-03 09:26:59 +0100

    add logo header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export function Header() {
   return (
-    <div className='w-full border-b md:w-3/4 mx-auto border-grass p-2 text-slate-900 text-center bg-slate-50'>
+    <div className='flex items-center justify-between w-full border-b md:w-3/4 mx-auto border-grass p-2 text-slate-900 text-center bg-slate-50'>
+      <Link href='/'>
+        <Image src={`/classik-games.png`} alt='logo' width={80} height={80} />
+      </Link>
       <Link href='/'>
         <p className='text-2xl font-extrabold italic text-transparent bg-clip-text bg-gradient-to-t from-yellow-200 to-yellow-500'>
           Football Classik Games
