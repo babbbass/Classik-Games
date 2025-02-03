8cd84292a43be7a1324312ab4714cc4dffcee44e
sebastien <sebastien.savan@gmail.com>
2025-02-03 14:32:39 +0100

    change header footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,56 +4,58 @@ import Link from "next/link"
 
 export function Footer() {
   return (
-    <div className='min-h-20 w-full border-t md:w-3/4 mx-auto border-grass p-2 text-slate-900 bg-slate-50 flex items-start justify-between'>
-      <div className='flex flex-col gap-2'>
-        <h4 className='uppercase mb-3 text-gray-600 text-xs font-bold'>
-          Classik Games
-        </h4>
-        <ul className='flex flex-col gap-4 text-xs'>
-          <li className='hover:underline'>
-            <Link href=''>{`Conditions d'utilisation`}</Link>
-          </li>
-          <li className='hover:underline'>
-            <Link href=''>Politique de confidentialité</Link>
-          </li>
-          <li className='hover:underline'>
-            <Link href=''>Contact</Link>
-          </li>
-          <li className='hover:underline'>
-            <Link href=''>RGPD</Link>
-          </li>
-        </ul>
+    <footer className='min-h-20 w-full border-t md:w-3/4 mx-auto border-grass p-2 text-slate-900 bg-slate-50'>
+      <div className='flex items-start justify-between max-w-7xl w-full mx-auto h-full'>
+        <div className='flex flex-col gap-2'>
+          <h4 className='uppercase mb-3 text-gray-600 text-xs font-bold'>
+            Classik Games
+          </h4>
+          <ul className='flex flex-col gap-4 text-xs'>
+            <li className='hover:underline'>
+              <Link href=''>{`Conditions d'utilisation`}</Link>
+            </li>
+            <li className='hover:underline'>
+              <Link href=''>Politique de confidentialité</Link>
+            </li>
+            <li className='hover:underline'>
+              <Link href=''>Contact</Link>
+            </li>
+            <li className='hover:underline'>
+              <Link href=''>RGPD</Link>
+            </li>
+          </ul>
+        </div>
+        <div className='flex flex-col gap-2'>
+          <h4 className='uppercase mb-3 text-gray-600 text-xs font-bold'>
+            Suivez Nous
+          </h4>
+          <ul className='flex flex-col gap-4'>
+            <li>
+              <a
+                href='https://www.instagram.com/classikgames/'
+                className='flex gap-2 items-center'
+              >
+                <Instagram size={18} />{" "}
+                <span className='text-xs hover:underline'>Instagram</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://www.tiktok.com/@classik.games'
+                className='flex gap-2'
+              >
+                <Image
+                  src='/tiktok.svg'
+                  alt='tiktok - classikgames'
+                  width={18}
+                  height={18}
+                />
+                <span className='text-xs hover:underline'>TikTok</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className='flex flex-col gap-2'>
-        <h4 className='uppercase mb-3 text-gray-600 text-xs font-bold'>
-          Suivez Nous
-        </h4>
-        <ul className='flex flex-col gap-4'>
-          <li>
-            <a
-              href='https://www.instagram.com/classikgames/'
-              className='flex gap-2 items-center'
-            >
-              <Instagram size={18} />{" "}
-              <span className='text-xs hover:underline'>Instagram</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://www.tiktok.com/@classik.games'
-              className='flex gap-2'
-            >
-              <Image
-                src='/tiktok.svg'
-                alt='tiktok - classikgames'
-                width={18}
-                height={18}
-              />
-              <span className='text-xs hover:underline'>TikTok</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    </footer>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,18 +3,27 @@ import Image from "next/image"
 
 export function Header() {
   return (
-    <div className='flex items-center justify-between w-full border-b md:w-3/4 mx-auto border-grass p-2 text-slate-900 text-center bg-slate-50'>
-      <Link href='/'>
-        <Image src={`/classik-games.png`} alt='logo' width={80} height={80} />
-      </Link>
-      <Link href='/'>
-        <p className='text-2xl font-extrabold italic text-transparent bg-clip-text bg-gradient-to-t from-yellow-200 to-yellow-500'>
-          Football Classik Games
-        </p>
-        <p className='text-xs italic'>
-          Trouve les joueurs des matchs de légendes
-        </p>
-      </Link>
-    </div>
+    <>
+      <header className='w-full border-b md:w-3/4 mx-auto border-grass p-2 text-slate-900 bg-slate-50'>
+        <div className='flex items-center max-w-7xl justify-between gap-2'>
+          <Link href='/'>
+            <Image
+              src={`/classik-games.png`}
+              alt='logo'
+              width={80}
+              height={80}
+            />
+          </Link>
+          <Link href='/'>
+            <p className='text-lg md:text-2xl font-extrabold italic text-transparent bg-clip-text bg-gradient-to-t from-yellow-200 to-yellow-500'>
+              Football Classik Games
+            </p>
+            <p className='text-xs italic'>
+              Trouve les joueurs des matchs de légendes
+            </p>
+          </Link>
+        </div>
+      </header>
+    </>
   )
 }
